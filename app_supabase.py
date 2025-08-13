bc4685c72bcfe7c95b3d44ed53726d8cd5669271
InCoolUser <libertyameri1@proton.me>
2025-08-13 10:50:30 -0400

    Fix socket error

--- app_supabase.py
+++ app_supabase.py
@@ -580,4 +580,6 @@ if __name__ == '__main__':
     initialize_supabase_models()
     
     # Start the application
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug_mode = os.environ.get('FLASK_ENV') == 'development'
+    socketio.run(app, debug=debug_mode, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
